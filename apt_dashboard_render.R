4575a1f6926fb6501b3dcf2cae96c7ea3dc8cda3
Rainer Koelle <rainer.koelle@eurocontrol.int>
2020-06-16 21:00:46 +0200

    apt_dashboard_04.Rmd development frozen; apt_dashboard_05.Rmd final versino for release 1; smaller changes to render.R

--- apt_dashboard_render.R
+++ apt_dashboard_render.R
@@ -29,13 +29,19 @@ min_year <- 2016 # by definition 5 years, i.e. 2016-2020
 # TO LIMIT TEST LOAD SUBSET FOR "TEST" AIRPORTS
 # To-Do: deployed version should use airport list, etc or be derived from ids
 # the list is used as an iterator for the render loop below
-apts   <- c("EGLL", "EDDF", "EIDW")
+#apts   <- c("EGLL", "EDDF", "EIDW")
+apts <- c("EBBR", "LEMD")
 # version - counter increased to 05 (including Sara's comments) & discussion with
 # Enrico --> deactivated as we move towards deployment. version<- "05"
 ## ------------------------------------------------ REMOVE WHEN DEPLOYED
 
 
 ## ------------ READ IN DATA TABLES FROM DOWNLOAD POINT ------------------
+
+# Thierry's dashboard table
+db_df <- readr::read_csv2("./data/STAT_AIRPORT_MONTHLY.csv")
+
+db_conf <- readr::read_csv2("./data/STAT_AIRPORT_CONFIGURATION.csv")
 
 # traffic counts and ids
 tfc_df <- readxl::read_excel("./data/Airport_Traffic.xlsx", sheet = "DATA"
@@ -44,6 +50,11 @@ tfc_df <- readxl::read_excel("./data/Airport_Traffic.xlsx", sheet = "DATA"
   mutate(FLT_DATE = lubridate::date(FLT_DATE))
 
 ids_df <- tfc_df %>% select(apt = APT_ICAO, name = APT_NAME, state = STATE_NAME)
+
+config_df <- db_conf %>%
+  na.omit() %>%    # currently only 2019 data, rest NA
+  select(APT_ICAO, YEAR, CONFIGURATION, SHARE_PCT) %>%
+  arrange(desc(SHARE_PCT))
 
 thru_df <- readr::read_csv("./data-test/STAT_AIRPORT_DATA_THRU.csv") %>% rename(APT_ICAO = APT)
 
@@ -72,16 +83,35 @@ slot_df <- readxl::read_excel("./data/ATFM_Slot_Adherence.xlsx", sheet = "DATA")
 
 asma_df <- readxl::read_excel("./data/ASMA_Additional_Time.xlsx", sheet = "DATA")
 
-txot_df <- readxl::read_excel("./data-test/Taxi-Out_Additional_Time.xlsx", sheet = "DATA")
+txot_df <- readxl::read_excel("./data/Taxi-Out_Additional_Time.xlsx", sheet = "DATA")
 
-txit_df <- readr::read_csv("./data-test/STAT_AIRPORT_DATA_TXIT.csv") %>% rename(APT_ICAO = APT)
+#txit_df <- readr::read_csv("./data-test/STAT_AIRPORT_DATA_TXIT.csv") %>% rename(APT_ICAO = APT)
+txit_df <- db_df %>%
+  select( APT_ICAO, YEAR, MONTH_NUM
+         ,N_SMPL  = NB_TAXI_IN_FL
+         ,TOT_REF = TAXI_IN_REF_TIME_MIN
+         ,TOT_ADD_TXIT = ADD_TAXI_IN_TIME_MIN)
 
-pddly_df<- readxl::read_excel("./data-test/STAT_AIRPORT_DATA.xlsx", sheet = "DATA") %>%
-  select(APT_ICAO, APT_IATA, YEAR, MONTH_NUM, APT_FLT_DEP
-         ,TOTAL_DLY_89, TOTAL_DLY_999, TOTAL_DLY_ZZZ
-         ,TOTAL_DLY_OTHER, TOTAL_UN_RPTED_DLY, TOTAL_OV_RPTED_DLY)
 
-turn_df <- readr::read_csv("./data-test/STAT_AIRPORT_DATA_TURN.csv") %>% rename(APT_ICAO = APT)
+#pddly_df<- readxl::read_excel("./data-test/STAT_AIRPORT_DATA.xlsx", sheet = "DATA") %>%
+#  select(APT_ICAO, APT_IATA, YEAR, MONTH_NUM, APT_FLT_DEP
+#         ,TOTAL_DLY_89, TOTAL_DLY_999, TOTAL_DLY_ZZZ
+#         ,TOTAL_DLY_OTHER, TOTAL_UN_RPTED_DLY, TOTAL_OV_RPTED_DLY)
+pddly_df <- db_df %>%
+  select(APT_ICAO, YEAR, MONTH_NUM, APT_FLT_DEP = NB_DLY_DEP_FL
+         ,TOTAL_DLY_89 = DLY_89_MIN, TOTAL_DLY_999 = DLY_999_MIN
+         ,TOTAL_DLY_ZZZ= DLY_ZZZ_MIN
+         ,TOTAL_DLY_OTHER = DLY_OTHER_MIN
+         ,TOTAL_UN_RPTED_DLY = UN_RPTED_DLY_MIN
+         ,TOTAL_OV_RPTED_DLY = OV_RPTED_DLY_MIN
+         )
+
+#turn_df <- readr::read_csv("./data-test/STAT_AIRPORT_DATA_TURN.csv") %>% rename(APT_ICAO = APT)
+turn_df <- readr::read_csv2("./data/STAT_AIRPORT_TURN_AROUND.csv") %>%
+  select(APT_ICAO, YEAR, MONTH_NUM, AC_CLASS
+         ,N = NB_TURN_ARROUND
+         ,TOT_SDTT_MIN, TOT_ACTT_MIN, TOT_ADTT_MIN) %>%
+  mutate(AVG_ATTT = TOT_ACTT_MIN / N, AVG_STTT = TOT_SDTT_MIN)
 
 punc_df <-  readr::read_csv("./data-test/STAT_AIRPORT_DATA_PUNC.csv") %>% rename(APT_ICAO = APT)
 
@@ -114,22 +144,23 @@ pick_apt_iata <- function(.df, .apt){
 apts %>%
   purrr::walk(
     .f=~rmarkdown::render(
-      input  = "apt_dashboard_04.Rmd"   # master flexdashboard Rmd
+      input  = "apt_dashboard_05.Rmd"   # master flexdashboard Rmd
       , params = list( #------ start params -------------------------
-                       icao = .
-                       ,iata = pick_apt_iata( pddly_df,  .apt = .)   # merge iata code with other source
-                       ,name = pick_apt_name(   tfc_df,  .apt = .)
-                       ,state= pick_state_name( tfc_df,  .apt = .)
-                       ,tfc  = filter_df_by_apt(tfc_df,  .apt = .)
-                       ,thru = filter_df_by_apt(thru_df, .apt = .)
-                       ,atfm = filter_df_by_apt(atfm_df, .apt = .)
-                       ,slot = filter_df_by_apt(slot_df, .apt = .)
-                       ,asma = filter_df_by_apt(asma_df, .apt = .)
-                       ,txot = filter_df_by_apt(txot_df, .apt = .)
-                       ,txit = filter_df_by_apt(txit_df, .apt = .)
-                       ,pddly= filter_df_by_apt(pddly_df,.apt = .)
-                       ,turn = filter_df_by_apt(turn_df, .apt = .)
-                       ,punc = filter_df_by_apt(punc_df, .apt = .)
+                        icao  = .
+                       ,iata  = pick_apt_iata(   db_df ,   .apt = .)   # merge iata code with other source
+                       ,name  = pick_apt_name(   tfc_df,   .apt = .)
+                       ,state = pick_state_name( tfc_df,   .apt = .)
+                       ,config= filter_df_by_apt(config_df,.apt = .)
+                       ,tfc   = filter_df_by_apt(tfc_df,   .apt = .)
+                       ,thru  = filter_df_by_apt(thru_df,  .apt = .)
+                       ,atfm  = filter_df_by_apt(atfm_df,  .apt = .)
+                       ,slot  = filter_df_by_apt(slot_df,  .apt = .)
+                       ,asma  = filter_df_by_apt(asma_df,  .apt = .)
+                       ,txot  = filter_df_by_apt(txot_df,  .apt = .)
+                       ,txit  = filter_df_by_apt(txit_df,  .apt = .)
+                       ,pddly = filter_df_by_apt(pddly_df, .apt = .)
+                       ,turn  = filter_df_by_apt(turn_df,  .apt = .)
+                       ,punc  = filter_df_by_apt(punc_df,  .apt = .)
       ) #----------------- end params ---------------------------
       # output_dir DEACTIVATED and included in output_file name
       # brittle as reported in stackoverflow
