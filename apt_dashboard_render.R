d0f5fdc6b33daeeec98b0798da113a1a03993d87
Rainer Koelle <rainer.koelle@eurocontrol.int>
2020-06-17 06:39:34 +0200

    data update and tweaks to renderer (landing page table) and associated dashboard presentation

--- apt_dashboard_render.R
+++ apt_dashboard_render.R
@@ -70,7 +70,10 @@ atfm_df <- readxl::read_excel("./data/Airport_Arrival_ATFM_Delay.xlsx", sheet = 
           ,AD_CAPACITY_ATC   = DLY_APT_ARR_C_1
           ,AD_STAFFING_ATC   = DLY_APT_ARR_S_1
           ,AD_EVENTS     = DLY_APT_ARR_P_1
-  )
+  ) %>%
+  # trim payload
+  select( APT_ICAO, YEAR, MONTH_NUM, FLT_DATE, FLT_ARR_1, DLY_APT_ARR_1
+         ,starts_with("AD_"), FLT_ARR_1_DLY, FLT_ARR_1_DLY_15)
 
 # idea was to read out the meta data to map causes to groups
 #atfm_reg <- readxl::read_excel("./data-test/Airport_Arrival_ATFM_Delay.xlsx", sheet = "META"
@@ -138,6 +141,88 @@ pick_apt_iata <- function(.df, .apt){
   iata <- iata$APT_IATA[1]
 }
 
+landing_page_indicators <- function(.df=db_df, .atfm=atfm_df, .apt){
+  inds <- .df %>% filter(APT_ICAO == .apt)
+
+  ind_tfc_2019 <- inds %>%
+    select(APT_ICAO, YEAR, NB_NM_TOT) %>% filter(YEAR == 2019) %>%
+    group_by(APT_ICAO, YEAR) %>%
+    summarise(NB_NM_TOT = sum(NB_NM_TOT, na.rm = TRUE)) %>% ungroup()
+
+  ind_txot_2019 <- inds %>% filter(YEAR == 2019) %>%
+    group_by(APT_ICAO, YEAR) %>%
+    summarise( ADD_TAXI_OUT_TIME_MIN = sum(ADD_TAXI_OUT_TIME_MIN, na.rm = TRUE)
+              ,NB_TAXI_OUT_FL        = sum(NB_TAXI_OUT_FL,        na.rm = TRUE)
+              )%>% ungroup() %>%
+    mutate(AVG_ADD_TXOT = round(ADD_TAXI_OUT_TIME_MIN / NB_TAXI_OUT_FL,2) ) %>%
+    select(AVG_ADD_TXOT)
+
+  ind_asma_2019 <- inds %>% filter(YEAR == 2019) %>%
+    group_by(APT_ICAO, YEAR) %>%
+    summarise( ADD_ASMA_TIME_MIN = sum(ADD_ASMA_TIME_MIN, na.rm = TRUE)
+               ,NB_ASMA_FL       = sum(NB_ASMA_FL,        na.rm = TRUE)
+    )%>% ungroup() %>%
+    mutate(AVG_ADD_ASMA = round(ADD_ASMA_TIME_MIN / NB_ASMA_FL,2) ) %>%
+    select(AVG_ADD_ASMA)
+
+  ind_atfm_2019 <- .atfm %>% filter(APT_ICAO == .apt, YEAR == 2019) %>%
+    select(FLT_ARR_1, DLY_APT_ARR_1) %>%
+    summarise( FLT_ARR_1     = sum(FLT_ARR_1,     na.rm = TRUE)
+              ,DLY_APT_ARR_1 = sum(DLY_APT_ARR_1, na.rm = TRUE)) %>%
+    mutate(AVG_ARR_ATFM = round(DLY_APT_ARR_1 / FLT_ARR_1,2) ) %>%
+    select(AVG_ARR_ATFM)
+
+  out <- ind_tfc_2019 %>%
+    bind_cols(ind_txot_2019, ind_asma_2019, ind_atfm_2019)
+}
+
+latest_month_indicators <- function(.df=db_df, .atfm=atfm_df, .apt){
+  inds <- .df %>% filter(APT_ICAO == .apt)
+
+  mth_name <- c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
+
+  ind_tfc_lm <- inds %>%
+    select(APT_ICAO, YEAR, MONTH_NUM, NB_NM_TOT) %>% na.omit() %>%
+    filter(YEAR == max(YEAR)) %>% filter(MONTH_NUM == max(MONTH_NUM)) %>%
+    mutate( MONTH = mth_name[MONTH_NUM]
+           ,  TFC = paste0(NB_NM_TOT," (", MONTH, " ", YEAR,")")
+           ) %>%
+    select(APT_ICAO, TFC)
+
+  ind_txot_lm <- inds %>%
+    select(APT_ICAO, YEAR, MONTH_NUM, ADD_TAXI_OUT_TIME_MIN, NB_TAXI_OUT_FL) %>%
+    na.omit() %>% filter(YEAR == max(YEAR) ) %>%
+    filter(MONTH_NUM == max(MONTH_NUM)) %>%
+    mutate(AVG_ADD_TXOT = round(ADD_TAXI_OUT_TIME_MIN / NB_TAXI_OUT_FL,2)
+           ,AVG_ADD_TXOT= paste0(AVG_ADD_TXOT," (", mth_name[MONTH_NUM], " ", YEAR,")")
+           ) %>%
+    select(AVG_ADD_TXOT)
+
+  ind_asma_lm <- inds %>%
+    select(APT_ICAO, YEAR, MONTH_NUM, ADD_ASMA_TIME_MIN, NB_ASMA_FL) %>%
+    na.omit() %>% filter(YEAR == max(YEAR) ) %>%
+    filter(MONTH_NUM == max(MONTH_NUM)) %>%
+    mutate( AVG_ADD_ASMA = round(ADD_ASMA_TIME_MIN / NB_ASMA_FL, 2)
+           ,AVG_ADD_ASMA = paste0(AVG_ADD_ASMA," (", mth_name[MONTH_NUM], " ", YEAR,")")
+           ) %>%
+    select(AVG_ADD_ASMA)
+
+  ind_atfm_lm <- .atfm %>% filter(APT_ICAO == .apt) %>%
+    select(YEAR, MONTH_NUM, FLT_ARR_1, DLY_APT_ARR_1) %>%
+    filter(YEAR == max(YEAR)) %>% filter(MONTH_NUM == max(MONTH_NUM)) %>%
+    na.omit() %>%
+    group_by(YEAR, MONTH_NUM) %>%
+    summarise( FLT_ARR_1     = sum(FLT_ARR_1, na.rm = TRUE)
+              ,DLY_APT_ARR_1 = sum(DLY_APT_ARR_1, na.rm = TRUE)) %>%
+    mutate( AVG_ARR_ATFM = round(DLY_APT_ARR_1 / FLT_ARR_1, 2)
+           ,AVG_ARR_ATFM = paste0(AVG_ARR_ATFM," (", mth_name[MONTH_NUM], " ", YEAR,")")) %>%
+    select(AVG_ARR_ATFM)
+
+  inds_lm <- ind_tfc_lm %>%
+    bind_cols(ind_txot_lm, ind_asma_lm, ind_atfm_lm)
+
+}
+
 ## ------------ RENDER DASHBOARDS -------------------------------------
 
 
@@ -151,6 +236,8 @@ apts %>%
                        ,name  = pick_apt_name(   tfc_df,   .apt = .)
                        ,state = pick_state_name( tfc_df,   .apt = .)
                        ,config= filter_df_by_apt(config_df,.apt = .)
+                       ,ldgsum= landing_page_indicators(db_df, atfm_df, .apt = .)
+                       ,latest= latest_month_indicators(db_df, atfm_df, .apt = .)
                        ,tfc   = filter_df_by_apt(tfc_df,   .apt = .)
                        ,thru  = filter_df_by_apt(thru_df,  .apt = .)
                        ,atfm  = filter_df_by_apt(atfm_df,  .apt = .)
@@ -168,4 +255,5 @@ apts %>%
       , output_file = paste0("./boards/", ., ".html")
     )
   )
+
 
